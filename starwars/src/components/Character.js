ad86ee370c1453515bb84bc34c6a918dd919b1d0
tactnician <tactnician@gmail.com>
2019-02-15 11:27:29 -0500

    Added Character and CharacterList components, and lesss file

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Character extends Component{
+    constructor(props){
+        super(props);0
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
